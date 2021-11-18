efddb8692196f94d605671b7b3930439eaa1514e
kirshiyin89 <70095657+kirshiyin89@users.noreply.github.com>
2021-11-18 16:44:05 +0100

    Update UserRepositoryTestContainersTest.java

--- demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryTestContainersTest.java
+++ demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryTestContainersTest.java
@@ -2,28 +2,47 @@ package com.dbtesting.demo.repository;
 
 import com.dbtesting.demo.entity.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.util.Collection;
+import java.util.List;
 
 @Testcontainers
 @DataJpaTest
+@Sql(scripts = "/insert-data.sql")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTestContainersTest {
+    
+    @BeforeEach
+    void init() {
+        new MySQLContainer("mysql")
+                .withDatabaseName("test")
+                .withUsername("usr")
+                .withPassword("pass")
+                .start();
+    }
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
     void findUserByEmailTest() {
-        userRepository.save(new User(1l,"Kirshi", "developer", "kirshi@example.org"));
-        Collection<User> users = userRepository.findUserByEmail("kirshi@example.org");
+        List<User> users = (List<User>) userRepository.findAll();
         Assertions.assertFalse(users.isEmpty());
+        Assertions.assertTrue(users.size() == 2);
         Assertions.assertEquals("Kirshi", users.stream().filter(user -> user.getEmail().equals("kirshi@example.org")).findFirst().get().getName());
+    }
+
+    @Test
+    void findUsersByRoleTest() {
+        List<User> developers = userRepository.findUsersByRole("tester");
+        Assertions.assertFalse(developers.isEmpty());
     }
 
 }
