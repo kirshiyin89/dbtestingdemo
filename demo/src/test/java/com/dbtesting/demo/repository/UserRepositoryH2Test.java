acdae0626c660f01986adf15114bf71db2cfb537
kirshiyin89 <70095657+kirshiyin89@users.noreply.github.com>
2021-11-18 16:45:51 +0100

    Update UserRepositoryH2Test.java

--- demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryH2Test.java
+++ demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryH2Test.java
@@ -1,17 +1,37 @@
 package com.dbtesting.demo.repository;
 
 import com.dbtesting.demo.entity.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
-    @Query("SELECT u FROM User u where u.email = :email")
-    User findUserByEmail(@Param("email") String email);
+@DataJpaTest
+@ExtendWith(SpringExtension.class)
+@Sql(scripts = "/insert-data.sql")
+@Sql(scripts = "/cleanup-data.sql", executionPhase = AFTER_TEST_METHOD)
+public class UserRepositoryH2Test {
 
-    @Query("SELECT u FROM User u where u.role = :role")
-    List<User> findUsersByRole(@Param("role") String role);
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findUserByEmailTest() {
+        User user = userRepository.findUserByEmail("kirshi@example.org");
+        Assertions.assertNotNull(user);
+        Assertions.assertEquals("Kirshi", user.getName());
+    }
+
+    @Test
+    void findUsersByRoleTest() {
+        List<User> developers = userRepository.findUsersByRole("developer");
+        Assertions.assertFalse(developers.isEmpty());
+    }
 }
