6b961796151f55716972d78940b0066233e9ffd5
kirshiyin89 <70095657+kirshiyin89@users.noreply.github.com>
2021-12-04 19:21:01 +0100

    Update UserRepositoryH2Test.java

--- demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryH2Test.java
+++ demo/src/test/java/com/dbtesting/demo/repository/UserRepositoryH2Test.java
@@ -20,18 +20,24 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TES
 public class UserRepositoryH2Test {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserRepository userRepository;  
+        
+    @Test
+    void findUsersByNameLongerThanTest() {
+        List<User> users = userRepository.findUsersByNameLongerThan(4);
+        Assertions.assertEquals("Kirshi", users.stream().filter(user -> user.getName()).findFirst());
+    }    
 
     @Test
     void findUserByEmailTest() {
-        User user = userRepository.findAdultUserByEmail("kirshi@example.org");
+        User user = userRepository.findByEmail("kirshi@example.org");
         Assertions.assertNotNull(user);
         Assertions.assertEquals("Kirshi", user.getName());
     }
 
     @Test
     void findUsersByRoleTest() {
-        List<User> developers = userRepository.findUsersByRole("developer");
+        List<User> developers = userRepository.findByRole("developer");
         Assertions.assertFalse(developers.isEmpty());
     }
 }
